0dd75587763db9f8a6efe1fd115afc45d9906729
234mph <148212310+234mph@users.noreply.github.com>
2024-05-16 01:23:16 +0500

    Add files via upload

---
+++ spring/test-lesson1/src/main/java/de/telran/testlesson1/TestLesson1Application.java
@@ -0,0 +1,13 @@
+package de.telran.testlesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLesson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLesson1Application.class, args);
+	}
+
+}
---
+++ spring/test-lesson1/src/test/java/de/telran/testlesson1/TestLesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.testlesson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLesson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
